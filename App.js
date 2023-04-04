2a063dc7b47c5207024f53aee8763c0123688ebf
Axoalot <100242031+Axoalot@users.noreply.github.com>
2023-04-04 18:18:54 -0300

    projeto 75

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
+import Login from "./Screens/login";
+import { createSwitchNavigator,createAppContainer } from "react-navigation";
 
 import TabNavigator from "./components/tabNavigator"
 
@@ -26,8 +28,18 @@ export default class App extends Component {
   render() {
     const { fontLoaded } = this.state;
     if (fontLoaded) {
-      return <TabNavigator />;
+      return <AppContainer />;
     }
     return null;
   }
 }
+const AppSwitchNavigator = createSwitchNavigator({
+  Login:{screen:Login},
+  BottonTab:{screen:TabNavigator},
+
+},{
+  initialRouteName:"Login"
+}
+
+)
+const AppContainer = createAppContainer(AppSwitchNavigator)
